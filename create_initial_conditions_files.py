d254ad71b58e8e98e78b89de874f5645c3c982d9
BlackWarrior06v <curipch2@gmail.com>
2024-12-19 12:58:31 -0600

    More general and automatized version

--- create_initial_conditions_files.py
+++ create_initial_conditions_files.py
@@ -1,31 +1,47 @@
+basepar = 'binary_Angular.par'
+
+# Supposing every par filename ends with .par
+base = basepar[:-4] 
+print(base)
+
 # Read the file contents
-with open('binary_Angular.par', 'r') as file:
+with open(basepar, 'r') as file:
     lines = file.readlines()
+# Remove empty lynes
+lines = [line for line in lines if line.strip()] 
 
 # Define the numerical values to add
-rho = ["1.5d0","2.0d0"]
-MBH = ["2.0d0","6.0d0"]
-y_0 = ["5.0d0","10.0d0"]
-vx0 = ["0.05d0","0.1d0","0.15d0","0.2d0"]
+numerical_values = [["1.5d0","2.0d0"],                   #rho
+                    ["2.0d0","6.0d0"],                  #MBH
+                    ["5.0d0","10.0d0"],                 #y
+                    ["0.05d0","0.1d0","0.15d0","0.2d0"]]#v
 
-rhonames = ["15","20"]
-MBHnames = ["2","6"]
-y_0names = ["5","10"]
-vx0names = ["05","1","15","2"]
+# Define the labels of the final par filename
+labels_value = [["15","20"],  #rho
+          ["2","6"],          #MBH
+          ["5","10"],         #y
+          ["05","1","15","2"]]#v
 
-for i in range (len(rho)):
-    for j in range(len(MBH)):
-        for k in range(len(y_0)):
-            for l in range(len(vx0)):
+labels = ["rhoc2","MBH2","yBH","vxBH"]
+
+for i, label_value in enumerate(labels_value):
+    for value in label_value:
+        value = labels[i]+value
+print(labels_value)
+
+for i in range (len(numerical_values[0])):
+    for j in range(len(numerical_values[1])):
+        for k in range(len(numerical_values[2])):
+            for l in range(len(numerical_values[3])):
                 # Define the parameters of ur simulations
-                new_lines = [f"rhoc2={rho[i]}\n",
-                             f"MBH2={MBH[j]}\n",
-                             f"yBH={y_0[k]}\n",
-                             f"vxBH={vx0[l]}\n"]
+                new_lines = [f"{labels[0]}={numerical_values[0][i]}\n",
+                             f"{labels[1]}={numerical_values[1][j]}\n",
+                             f"{labels[2]}={numerical_values[2][k]}\n",
+                             f"{labels[3]}={numerical_values[3][l]}\n"]
                 
                 # Insert new lines before the last line
-                lines_new_files = lines[:-1] + new_lines + lines[-1:]
+                lines_new_files = lines[:-2] + new_lines + lines[-2:]
 
                 # Write the modified content to a new file
-                with open(f'binary_rho{rhonames[i]}_MBH{MBHnames[j]}_y{y_0names[k]}_v{vx0names[l]}.par', 'w') as file:
+                with open(f'{base}{labels_value[0][i]}_MBH{labels_value[1][j]}_y{labels_value[2][k]}_v{labels_value[3][l]}.par', 'w') as file:
                     file.writelines(lines_new_files)                    
